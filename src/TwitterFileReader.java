dec5b97c96024febb21be191cc2a684671a02ebb
Mike Mason <mmason@thoughtworks.com>
2011-12-17 12:39:14 -0600

    New README, smaller sample data

--- src/TwitterFileReader.java
+++ src/TwitterFileReader.java
@@ -3,6 +3,6 @@ import java.io.*;
 public class TwitterFileReader extends TwitterReader {
 
     BufferedReader getJsonReader() throws FileNotFoundException {
-        return new BufferedReader(new FileReader(new File("/Users/mgm/work/hack_day/TwitterStreamAdapter/data/world/world-geo-1.json")));
+        return new BufferedReader(new FileReader(new File("data/world/world-geo-sample.json")));
     }
 }
